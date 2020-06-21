2457f821f339ef91fbfe4dce98d3356e1712d6f9
thomasinfor <thomaswang2003@gmail.com>
2020-06-21 13:49:26 +0800

    jizz

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const express=require('express');
 const port=3000;
 const app=new express();
 
+app.use(express.static("public"));
 app.get('/', function(req, res){
     res.sendFile('./index.html',{"root":__dirname});
 }).listen(port,()=>{console.log('Server running at localhost:'+port);});
