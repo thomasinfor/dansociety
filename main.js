7b52389a12955e329cbf80d7f86702d1da069597
thomasinfor <thomaswang2003@gmail.com>
2020-06-23 14:34:37 +0800

    jizzz

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const express=require('express');
+const bodyParser = require('body-parser');
 
-const port=3000;
+const port=2003;
 const app=new express();
 
 app.use(express.static("public"));
-app.get('/', function(req, res){
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/',function(req,res){
     res.sendFile('./index.html',{"root":__dirname});
 }).listen(port,()=>{console.log('Server running at localhost:'+port);});
+
+app.post('/subscribe',(req,res)=>{
+	console.log(req.body);
+	res.send('jizz');
+})
