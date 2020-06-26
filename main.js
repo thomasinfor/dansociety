0cb3508452f2d48102bc9c16e64e98731ba570c9
thomasinfor <thomaswang2003@gmail.com>
2020-06-26 16:05:03 +0800

    db

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 const express=require('express');
-const bodyParser = require('body-parser');
+const bodyParser=require('body-parser');
+const mongo=require('mongodb').MongoClient;
 
+const dburl="mongodb://localhost:27017/";
 const port=2003;
 const app=new express();
 
+mongo.connect(dburl,function(err,db){
+  if(err) throw err;
+  var dbo=db.db('dansociety');
+  dbo.createCollection("subscribe-email",function(err,res){
+    if(err) throw err;
+    console.log("Collection 'subscribe-email' created !");
+    db.close();
+  });
+});
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/',function(req,res){
     res.sendFile('./index.html',{"root":__dirname});
 }).listen(port,()=>{console.log('Server running at localhost:'+port);});
 
 app.post('/subscribe',(req,res)=>{
-	console.log(req.body);
-	res.send('jizz');
-})
+  console.log(req.body);
+  mongo.connect(dburl,function(err,db){
+    if(err) throw err;
+    var dbo=db.db('dansociety');
+    dbo.collection("subscribe-email").insertOne({
+      email:req.body.email,time:new Date()
+    },function(err,res){
+      if(err) throw err;
+      db.close();
+    });
+  });
+  res.end();
+});
--- public/scripts.js
+++ public/scripts.js
@@ -1,56 +1,72 @@
 /*!
-    * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
+  * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
+  * Copyright 2013-2020 Start Bootstrap
+  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+  */
+  (function ($) {
+  "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length
+        ? target
+        : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 100,
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+
+  $('form').submit(function(event){
+    if($('#email').val()=='') return event.preventDefault();
+    $.post('/subscribe',{email: $('#email').val()},function(data,status){
+      $('#submit-btn').css('background-color','rgb(244, 67, 54)');
+      $('#submit-btn').css('cursor','auto');
+      $('#submit-btn').text('SUBSCRIBED');
+      setTimeout(function(){
+        $('#submit-btn').css('background-color','rgb(84, 139, 135)');
+        $('#submit-btn').css('cursor','pointer');
+        $('#submit-btn').text('SUBSCRIBE');
+        $('#email').val('');
+      },2000);
     });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
-    });
-
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+    event.preventDefault();
+  });
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
