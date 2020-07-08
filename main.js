185e3c37ecf8e78cc6ebe0ac7afceb212c643e0d
thomasinfor <thomaswang2003@gmail.com>
2020-07-08 21:03:57 +0800

    bug fix

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ const util=require('util');
 const mv=util.promisify(require('mv'));
 const path=require('path');
 const ffmpeg=require('ffmpeg');
-// const { exec }=require("child_process");
-const exec=require('child-process-promise').exec;
+const execute=require('child-process-promise').exec;
 // const posenet=require('@tensorflow-models/posenet');
 
 // const net=posenet.load({
@@ -26,12 +25,18 @@ const dburl="mongodb://localhost:27017/";
 const port=2003;
 const app=new express();
 
+// const log=(new console.Console({
+//   stdout: fs.createWriteStream('./log/stdout.log'),
+//   stderr: fs.createWriteStream('./log/stderr.log')
+// })).log;
+const log=console.log;
+
 // mongo.connect(dburl,function(err,db){
 //   if(err) throw err;
 //   var dbo=db.db('dansociety');
 //   dbo.createCollection("subscribe-email",function(err,res){
 //     if(err) throw err;
-//     console.log("Collection 'subscribe-email' created !");
+//     log("Collection 'subscribe-email' created !");
 //     db.close();
 //   });
 // });
@@ -40,10 +45,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',function(req,res){
     res.sendFile('./index.html',{"root":__dirname});
-}).listen(port,()=>{console.log('Server running at localhost:'+port);});
+}).listen(port,()=>{log('Server running at localhost:'+port);});
 
 app.post('/subscribe',(req,res)=>{
-  console.log(req.body);
+  log(req.body);
   // mongo.connect(dburl,function(err,db){
   //   if(err) throw err;
   //   var dbo=db.db('dansociety');
@@ -59,7 +64,7 @@ app.post('/subscribe',(req,res)=>{
 function content(x){x=String(x);return x.slice(0,x.length-3);}
 function status(x){x=String(x);return x.slice(x.length-3);}
 function moveVideo(oldpath,newpath){
-  console.log(newpath);
+  log(newpath);
   var video = new ffmpeg(oldpath);
   video.then(video=>{
     return video
@@ -70,7 +75,7 @@ function moveVideo(oldpath,newpath){
   return video;
 }
 function moveImg(oldpath,newpath){
-  console.log(newpath);
+  log(newpath);
   return mv(oldpath,newpath,{mkdirp: true});
 }
 async function updateToken(){
@@ -79,11 +84,11 @@ async function updateToken(){
           `-d "{\\"api_key\\": \\"${apikey}\\"}" `+
           `-X POST `+
           `-w "%{http_code}"`;
-  await exec(cmd).then(res=>{
+  await execute(cmd).then(res=>{
     if(status(res.stdout)==200){
-      console.log('token updated !');
+      log('token updated !');
       token=JSON.parse(content(res.stdout)).access_token;
-      console.log(token);
+      log(token);
     }else updateToken();
   }).catch(err=>{
     console.error('request token\nERROR: ',err);
@@ -98,13 +103,17 @@ function submitWrnch(path,main='centre',width=456,height=256){
           `-F "media=@${path}" `+
           `-X POST `+
           `-w "%{http_code}"`;
-  return exec(cmd).then(res=>{
+  return execute(cmd).then(res=>{
     var st=status(res.stdout);
     if(st==200||st==202){
       var id=JSON.parse(content(res.stdout)).job_id;
-      console.log(`wrnch job (id=${id}) submitted !`);
+      log(`wrnch job (id=${id}) submitted !`);
       return id;
-    }else return null;
+    }else{
+      log(cmd);
+      log(st,res.stdout);
+      return null;
+    }
   }).then(id=>{
     if(!id) return id;
     return new Promise((resolve,reject)=>{
@@ -112,9 +121,9 @@ function submitWrnch(path,main='centre',width=456,height=256){
         var cmd=`curl https://api.wrnch.ai/v1/status/${id} `+
                 `-H "Authorization: Bearer ${token}" `+
                 `-w "%{http_code}"`;
-        exec(cmd).then(res=>{
+        execute(cmd).then(res=>{
           if(content(res.stdout)==`"Processed"\n`){
-            console.log(`job ${id} Processed !`);
+            log(`job ${id} Processed !`);
             resolve(id);
             clearInterval(timer);
           }
@@ -132,12 +141,15 @@ function getResultWrnch(id){
   var cmd=`curl https://api.wrnch.ai/v1/jobs/${id} `+
           `-H "Authorization: Bearer ${token}" `+
           `-w "%{http_code}"`;
-  return exec(cmd).then(res=>{
+  return execute(cmd,{maxBuffer: 10*1024*1024}).then(res=>{
     var st=status(res.stdout);
     if(st==200){
-      console.log(`got job ${id} result !`);
+      log(`got job ${id} result !`);
       return JSON.parse(content(res.stdout));
-    }else return null;
+    }else{
+      log(cmd); log(st);
+      return null;
+    }
   }).catch(err=>{
     console.error(`get job ${id} result\nERROR: `,err);
   });
@@ -145,40 +157,50 @@ function getResultWrnch(id){
 function pose(p){
   var jsonp=`./pose/${path.parse(p).name}.json`;
   if(!fs.existsSync(jsonp)){
+    // log('new ',p);
     return submitWrnch(p).then(getResultWrnch).then(res=>{
-      fs.writeFile(jsonp,JSON.stringify(res,null,2),err=>{
-        if(err) throw err;
-        else console.log(`wrote ${jsonp}`);
-      });
-      return res;
+      if(res){
+        fs.writeFile(jsonp,JSON.stringify(res,null,2),err=>{
+          if(err) throw err;
+          else log(`wrote ${jsonp}`);
+        });
+        return res;
+      }else return null;
     });
   }else{
+    // log('old ',p);
     return new Promise((resolve,reject)=>{
       fs.readFile(jsonp,(err,data)=>{
         if(err) throw err;
-        console.log(`read ${jsonp}`);
+        log(`read ${jsonp}`);
         resolve(JSON.parse(data));
       });
     });
   }
 }
 function evaluate(p1,v1,p2,v2){
-  console.log(p1,v1,p2,v2);
+  log(p1,v1,p2,v2);
   return Promise.all([pose(p1),pose(v1),pose(p2),pose(v2)]).then(values=>{
     if(values[0]==null) return null;
     return algo.evaluate_loss(values[0],values[1],values[2],values[3]);
   }).catch(err=>{
-    console.log(`evaluate\n${p1}\n${v1}\n${p2}\n${v2}\nERROR: ${err}`);
+    log(`evaluate\n${p1}\n${v1}\n${p2}\n${v2}\nERROR: ${err}`);
   });
 }
-
+setTimeout(()=>{evaluate(
+  './public/demo/demo-pict.png',
+  './public/demo/demo-video.mp4',
+  './data/1594181061895.png',
+  './data/1594181085057.mp4').then(pts=>{
+    log(pts);
+  })},5000);
 app.post('/demo/evaluate',(req,res)=>{
-  console.log('post evaluate');
+  log('post evaluate');
   const form = formidable({ multiples: true });
   form.parse(req, (err, fields, files) => {
     if(err){next(err);return;}
-    // console.log(fields);
-    // console.log(files);
+    // log(fields);
+    // log(files);
     try{
       var pimg=path.join(path.join(__dirname,'./data/'),files.userImg.name);
       var pvideo=path.join(path.join(__dirname,'./data/'),files.userVideo.name+'.mp4');
@@ -191,12 +213,12 @@ app.post('/demo/evaluate',(req,res)=>{
           pimg,pvideo
         );
       }).then(pts=>{
-        console.log(pts);
+        log(pts);
         res.end(String(pts));
-        console.log('post evaluate end');
+        log('post evaluate end');
       }).catch(e=>{
-        console.log(e);
+        log(e);
       });
-    }catch(e){console.log(e);}
+    }catch(e){log(e);}
   });
 });
